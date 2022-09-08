ebf1aa70b5a11e7708dacbad422caf0450cdfdc2
E-J124 <ezehjustine428@gmail.com>
2022-09-08 13:32:34 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
+#include <stdio.h>  
 /**
- * main- Services as the entry point for the program
- *
- * Return: By default returns zero if no errors otherwise returns non-zero value.
+ * main - entry point
+ * Return: 0
  */
-int main(void)	
+int main(void)
 {	
-  puts("\"Programming is like building a multilingual puzzle");	
-  return (0);
-}  
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
