ed4eeeef8219f7b5325e3fba00c4bec3d2e71908
E-J124 <ezehjustine428@gmail.com>
2022-09-28 17:57:04 -0700

    task 0 and 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-* _print_rev_recursion - reverses a string
-* @s: input
-* Return: Always 0
-*/
+ * _print_rev_recursion - reverses a string
+ * @s: input
+ * Return: Always 0
+ */
 void _print_rev_recursion(char *s)
 {
 	if (*s)
